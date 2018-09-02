71e77a576fbe319249baf81797fd618641e436df
Taylan <tayselvi@gmail.com>
2018-09-02 23:32:47 +1000

    -

--- src/com/arlania/world/entity/impl/player/PlayerHandler.java
+++ src/com/arlania/world/entity/impl/player/PlayerHandler.java
@@ -240,24 +240,9 @@ break;
         	//player.setReceivedStarter(true);
         }
 		//DialogueManager.start(player, 177);
-        player.getPacketSender().sendMessage("@blu@Welcome to Morytania! We hope you enjoy your stay!");
-
-        
-        if (player.experienceLocked()) {
-            player.getPacketSender().sendMessage("@red@Warning: your experience is currently locked.");
-        }
-        ClanChatManager.handleLogin(player);
-        ClanChatManager.join(player, "help");
-
-        if (GameSettings.BONUS_EXP) {
-			player.getPacketSender().sendMessage("<img=10> <col=008FB2>Morytania currently has a bonus experience event going on, make sure to use it!");
-        }
-        if (WellOfWealth.isActive()) {
-            player.getPacketSender().sendMessage("<img=10> @blu@The Well of Wealth is granting x2 Easier Droprates for another " + WellOfWealth.getMinutesRemaining() + " minutes.");
-        }
-        if (WellOfGoodwill.isActive()) {
-            player.getPacketSender().sendMessage("<img=10> @blu@The Well of Goodwill is granting 30% Bonus xp for another " + WellOfGoodwill.getMinutesRemaining() + " minutes.");
-        }
+        player.getPacketSender().sendMessage("Welcome to <shad=fff600>Rune Rebellion</shad>.");
+       // ClanChatManager.handleLogin(player);
+        //ClanChatManager.join(player, "help");
         PlayerPanel.refreshPanel(player);
 
     	//New player
@@ -268,14 +253,6 @@ break;
 
         player.getPacketSender().updateSpecialAttackOrb().sendIronmanMode(player.getGameMode().ordinal());
 
-        if (player.getRights() == PlayerRights.OWNER || player.getRights() == PlayerRights.SUPPORT || player.getRights() == PlayerRights.MODERATOR || player.getRights() == PlayerRights.ADMINISTRATOR || player.getRights() == PlayerRights.DEVELOPER) {
-            World.sendMessage("<img="+player.getRights().ordinal()+"><col=6600CC> " + Misc.formatText(player.getRights().toString().toLowerCase()) + " " + player.getUsername() + " has just logged in, feel free to message them for support.");
-        }
-        if (player.getRights() == PlayerRights.MODERATOR || player.getRights() == PlayerRights.ADMINISTRATOR ||  player.getRights() == PlayerRights.SUPPORT || player.getRights() == PlayerRights.DEVELOPER || player.getRights() == PlayerRights.OWNER) {
-			if (!StaffList.staff.contains(player.getUsername())) {
-				StaffList.login(player);
-			}
-		}
         GrandExchange.onLogin(player);
 
         if (player.getPointsHandler().getAchievementPoints() == 0) {
@@ -283,7 +260,7 @@ break;
         }
         
         if(player.getPlayerOwnedShopManager().getEarnings() > 0) {
-        	player.sendMessage("<col=FF0000>You have unclaimed earnings in your player owned shop!");
+        	player.sendMessage("You have unclaimed earnings in your player owned shop!");
         }
 
         PlayerLogs.log(player.getUsername(), "Login from host " + player.getHostAddress() + ", serial number: " + player.getSerialNumber());
